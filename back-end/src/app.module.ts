4b82cfb6c674db1458597c7efa163dd800016d54
christian <christian@enclave.vn>
2020-07-22 14:04:47 +0700

    fix gitignore

---
+++ back-end/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [TrackModule, AlbumModule, ArtistModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
