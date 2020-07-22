8458c0758fefd1aa226727a928573499aeb6675b
christian <christian@enclave.vn>
2020-07-22 17:12:31 +0700

    temp

--- back-end/src/config/api.config.ts
+++ back-end/src/config/api.config.ts
@@ -1,18 +1,24 @@
 import qs from 'qs';
 import { PathLike } from 'fs';
+import { appConfig } from './app.config';
+
+const { apiUrl, rapidApiHost, rapidApiKey } = appConfig;
 
 export const apiConfig = {
     returnRejectedPromiseOnError: true,
     withCredentials: true,
     timeout: 30000,
-    baseURL: "https://jsonplaceholder.typicode.com/",
+    baseURL: apiUrl,
     headers: {
         common: {
             "Cache-Control": "no-cache, no-store, must-revalidate",
             Pragma: "no-cache",
             "Content-Type": "application/json",
             Accept: "application/json",
-        },
+            "x-rapidapi-host": rapidApiHost,
+            "x-rapidapi-key": rapidApiKey,
+            "useQueryString": true
+        }
     },
     paramsSerializer: (params: PathLike) => qs.stringify(params, { indices: false })
 }
