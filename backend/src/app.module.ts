92a1cea2a99524a7cfdd1fc940adccd07f3cbc0c
christian <christian@enclave.vn>
2020-07-22 13:19:04 +0700

    init modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
-  imports: [],
+  imports: [TrackModule, AlbumModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
