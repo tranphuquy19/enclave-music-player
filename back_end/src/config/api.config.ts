c761d3d74b191de2434fbc51a85b1886d8f0be86
christian <christian@enclave.vn>
2020-07-24 14:29:32 +0700

    axios

--- back_end/src/config/api.config.ts
+++ back_end/src/config/api.config.ts
@@ -1,18 +1,14 @@
-import qs from 'qs';
-import { PathLike } from 'fs';
 import { appConfig } from './app.config';
+import { AxiosRequestConfig } from 'axios';
 
 const { rapidApiHost, rapidApiKey } = appConfig;
 
-export const apiConfig = {
-    returnRejectedPromiseOnError: true,
-    withCredentials: true,
-    timeout: 30000,
+export const apiConfig: AxiosRequestConfig = {
+    timeout: 3000,
     baseURL: rapidApiHost,
     headers: {
         "Content-Type": "application/json",
         "x-rapidapi-host": rapidApiHost,
         "x-rapidapi-key": rapidApiKey,
     },
-    paramsSerializer: (params: PathLike) => qs.stringify(params, { indices: false })
 }
