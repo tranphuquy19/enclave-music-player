47db9d28493de09f33ce350374300d0ff4c5c1c6
Tran Van Hong Quan <hongquanhak@gmail.com>
2020-08-13 19:30:57 +0700

    Updates

---
+++ precache-manifest.55f86ce6be819b37de475f257ed1e9fa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21887610fc3ff02f9e6ee30ba6b8ba1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "027bc9c51b858839119c",
+    "url": "/static/css/2.3ce6e682.chunk.css"
+  },
+  {
+    "revision": "63318e6e4ede8875c494",
+    "url": "/static/css/main.51177b0f.chunk.css"
+  },
+  {
+    "revision": "027bc9c51b858839119c",
+    "url": "/static/js/2.a125f2b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a125f2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63318e6e4ede8875c494",
+    "url": "/static/js/main.620d7819.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "7fcbaaf2e532f2e4808d2c652e8f26b4",
+    "url": "/static/media/logo.7fcbaaf2.png"
+  },
+  {
+    "revision": "2395eadc34cc7965d2c412152fa06a9b",
+    "url": "/static/media/moon.2395eadc.svg"
+  },
+  {
+    "revision": "03b952a4c1374d5b105b411e693bba1d",
+    "url": "/static/media/sun.03b952a4.svg"
+  }
+]);
