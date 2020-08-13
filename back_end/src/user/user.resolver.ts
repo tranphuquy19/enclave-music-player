565c32db360954dc4a76a597bd9d01a0d0f32f83
tranphuquy19 <tranphuquy19@gmail.com>
2020-08-13 23:56:38 +0700

    add auth graphql

--- back_end/src/user/user.resolver.ts
+++ back_end/src/user/user.resolver.ts
@@ -1,13 +1,25 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UserService } from './user.service';
-import { UserRO } from './user.dto';
+import { UserRO, UserDTO, UserLoginRegister } from './user.dto';
 
 @Resolver('User')
 export class UserResolver {
     constructor(private userService: UserService) { }
 
     @Query()
-    users() : Promise<UserRO[]>{
+    users(): Promise<UserRO[]> {
         return this.userService.showAll();
+    }
+
+    @Mutation()
+    login(@Args() { username, password }: UserLoginRegister): Promise<UserRO> {
+        const user: UserDTO = { username, password };
+        return this.userService.login(user);
+    }
+
+    @Mutation()
+    register(@Args() { username, password }: UserLoginRegister): Promise<UserRO> {
+        const user: UserDTO = {username, password};
+        return this.userService.register(user);
     }
 }
