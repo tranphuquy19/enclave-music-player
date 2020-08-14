f285d5df93f1a3e5f8a92b39f4a7b537b09b2553
tranphuquy19 <tranphuquy19@gmail.com>
2020-08-14 21:04:51 +0700

    config react-redux

---
+++ enclave-project-js/src/shared/Types.js
@@ -0,0 +1,13 @@
+/*
+ * Created by @tranphuquy19 on 14/08/2020
+ * @author: tranphuquy19@gmail.com
+ */
+const USER_LOGIN = 'USER_LOGIN';
+const USER_LOGOUT = 'USER_LOGOUT';
+const USER_REGISTER = 'USER_REGISTER';
+
+export {
+    USER_LOGIN,
+    USER_LOGOUT,
+    USER_REGISTER
+}
---
+++ enclave-project-js/src/store/reducer/index.js
@@ -0,0 +1,10 @@
+/*
+ * Created by @tranphuquy19 on 14/08/2020
+ * @author: tranphuquy19@gmail.com
+ */
+
+import {combineReducers} from "redux";
+
+const masterReducer = combineReducers({});
+
+export default masterReducer;
